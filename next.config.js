a7c89c18c97e3a1394862c1169d8c51daaf789f1
technostacksgit <146938191+technostacksgit@users.noreply.github.com>
2024-11-22 10:35:18 +0530

    Update and rename next.config.ts to next.config.js
    Updated content and renamed to .js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
   images: {
     domains: ['next-project-image-upload-testing.s3.us-east-1.amazonaws.com'],
+    unoptimized: true // Add this for static export
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
